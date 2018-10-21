01d20022bc73302ab39cf508e373192623c8ee3c
Ashwin <aswinjeganath@gmail.com>
2018-10-21 16:58:19 +0530

    End of lecture 15

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,17 @@
-const mutations = {};
+const mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if user is authenticated
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info,
+    );
+
+    return item;
+  },
+};
 
 module.exports = mutations;
